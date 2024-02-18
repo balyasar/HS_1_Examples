2012c5ef647831956be621eb1ce2c953c4c110a3
yasar.bal <ysr.bal@gmail.com>
2024-02-18 17:15:53 +0300

    18/02/2024 gün sonu

---
+++ src/com/yasar/lesson010/interfaceDemo/KrediHesabi.java
@@ -0,0 +1,9 @@
+package com.yasar.lesson010.interfaceDemo;
+
+public class KrediHesabi extends Hesap {
+    public KrediHesabi(String hesapNo, double bakiye, String kullaniciIsmi) {
+        super(hesapNo, bakiye, kullaniciIsmi);
+    }
+
+
+}
